cfc2559e036cd631c73f0ded3a2291f9cde064f1
Amol shelke <shelkeamol897@gmail.com>
2022-10-28 00:45:16 +0530

    Expense Tracker React init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./component/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2>Title</h2>
+        <div>Amount</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
