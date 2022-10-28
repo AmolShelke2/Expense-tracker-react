994e8329dd6a4c9d7aa6e62014cb4dc5140d1ff0
Amol shelke <shelkeamol897@gmail.com>
2022-10-28 13:45:01 +0530

    Added styles for ExpenseItem component

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,10 +1,12 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
   return (
-    <div>
-      <div>Date</div>
-      <div>
-        <h2>Title</h2>
-        <div>Amount</div>
+    <div className="expense-item">
+      <div>28 oct 22</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">10,000</div>
       </div>
     </div>
   );
---
+++ src/component/ExpenseItem2.jsx
@@ -0,0 +1,15 @@
+import "./ExpenseItem2.css";
+
+function ExpenseItem2() {
+  return (
+    <div className="expense-item2">
+      <div>28 oct 22</div>
+      <div className="expense-item2__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item2__price">10, 000</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem2;
