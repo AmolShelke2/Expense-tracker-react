7bd2a79a143be0d625105b972b3ae014f6b61d10
Amol shelke <shelkeamol897@gmail.com>
2022-10-31 21:46:01 +0530

    Added Splitting component into multiple component created Expense Date component

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,17 +1,11 @@
+import ExpneseDate from './ExpenseDate';
+
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('en-US', { month: 'long' });
-  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-  const year = props.date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpneseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
@@ -21,5 +15,3 @@ function ExpenseItem(props) {
 }
 
 export default ExpenseItem;
-
-// Splitting Component in multiple component
