72b030c5eb3c216994ea51934f5c784ca7003219
Amol shelke <shelkeamol897@gmail.com>
2022-11-04 19:31:51 +0530

    Update Listening to user Input

--- src/component/newExpense/ExpenseForm.js
+++ src/component/newExpense/ExpenseForm.js
@@ -3,12 +3,16 @@ import React from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+  const titleChangeHandler = event => {
+    console.log(event.target.value);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text " />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
 
         <div className="new-expense__control">
