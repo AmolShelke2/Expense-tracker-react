17df374dfc085998d9355e4fd7d4e4286c4c512c
Amol shelke <shelkeamol897@gmail.com>
2022-11-04 21:52:37 +0530

    Removed extra console.loogs and updating states which is depend on the previous state

--- src/component/expenses/ExpenseItem.js
+++ src/component/expenses/ExpenseItem.js
@@ -7,11 +7,9 @@ import './ExpenseItem.css';
 
 const ExpenseItem = props => {
   const [title, setTitle] = useState(props.title);
-  console.log('ExpenseItem Evaluated by React');
 
   const clickHandler = () => {
     setTitle('Updated');
-    console.log(title);
   };
 
   return (
