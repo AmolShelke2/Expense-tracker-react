30eb416a5a84b590c7404b1b425c549ede5d28a2
Amol shelke <shelkeamol897@gmail.com>
2022-11-09 14:03:20 +0530

    Adde Usestate for expenses and adding new expenses into the state

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import NewExpense from './component/newExpense/NewExpense';
 import Expenses from './component/expenses/Expenses';
 
+const INITAL_STATE = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 199,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 2999,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 4500,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 199,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 2999,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 4500,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(INITAL_STATE);
 
   const addNewExpenseHandler = newExpense => {
-    console.log('In App.js');
-    console.log(newExpense);
+    setExpenses(prevExpenses => {
+      return [newExpense, ...prevExpenses];
+    });
   };
 
   // using react DOM
